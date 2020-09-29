6bf5bbd23bed1a0bfe0c1614744aa4de4859dbe4
Nikog Samonic <Nikog.Samonic@gmail.com>
2020-09-29 01:44:23 +0100

    Update index.php
    Add html template.

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,19 @@
+<!doctype html> 
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Template</title>
+    <meta name="description" content="The HTML5 Template">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+  </head>
+
+  <body>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
 <?php
 
